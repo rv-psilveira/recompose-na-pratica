eb5992019dd9e238f3dca7a23dcf2630af299219
Paulo Silveira <psilveira@redventures.com>
2018-05-11 08:37:16 -0300

    New style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,9 +3,18 @@ import Carousel from 'components/carousel'
 const Home = () =>
   <div className="container-carousel">
 		<Carousel>
-			<div className="item">item 1</div>
-			<div className="item">item 2</div>
-			<div className="item">item 3</div>
+			<div className="item">
+        <img src="/assets/iq-vermelho.png" alt=""/>
+        item 1
+      </div>
+			<div className="item">
+        <img src="/assets/iq-verde.png" alt=""/>
+        item 2
+      </div>
+			<div className="item">
+        <img src="/assets/iq-azul.png" alt=""/>
+        item 3
+      </div>
 		</Carousel>
 	</div>
 
